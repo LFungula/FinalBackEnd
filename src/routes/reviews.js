5cf870e1f4539e3a1225fc4a6f2560dbe36ccd3d
LIN FUNGULA <LINDA.FUNGULA@GMAIL.COM>
2025-07-04 10:32:10 +0200

    Add code Review Routes

---
+++ src/routes/reviews.js
@@ -0,0 +1,79 @@
+import { Router } from "express";
+import getReviews from "../services/reviews/getReviews.js";
+import getReviewByID from "../services/reviews/getReviewByID.js";
+import updateReviewByID from "../services/reviews/updateReviewByID.js";
+import deleteReviewByID from "../services/reviews/deleteReviewByID.js";
+import createReview from "../services/reviews/createReview.js";
+//import auth from "../middleware/auth.js";
+
+const router = Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { userId, propertyId, rating, comment } = req.body;
+    const newReview = await createReview(userId, propertyId, rating, comment);
+    res.status(201).json(newReview);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const { location, pricePerNight } = req.body;
+    const reviews = await getReviews(location, pricePerNight);
+    res.status(200).json(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const review = await getReviewByID(id);
+
+    if (!review) {
+      res.status(404).json({ message: `Review with id ${id} not found` });
+    }
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const review = await deleteReviewByID(id);
+
+    if (!review) {
+      res.status(404).json({ message: `Review with id ${id} not found` });
+    }
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { userId, propertyId, rating, comment } = req.body;
+    const review = await updateReviewByID(id, {
+      userId,
+      propertyId,
+      rating,
+      comment,
+    });
+
+    if (!review) {
+      res.status(404).json({ message: `Review with id ${id} not found` });
+    }
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
