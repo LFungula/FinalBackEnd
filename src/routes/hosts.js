9769319891d171fcb5e8e1887990b3021fba0257
LIN FUNGULA <LINDA.FUNGULA@GMAIL.COM>
2025-07-16 14:58:23 +0200

    Add Host checks post / put

--- src/routes/hosts.js
+++ src/routes/hosts.js
@@ -5,8 +5,57 @@ import updateHostByID from "../services/hosts/updateHostByID.js";
 import deleteHostByID from "../services/hosts/deleteHostByID.js";
 import createHost from "../services/hosts/createHost.js";
 import auth from "../middleware/auth.js";
+import validator from "validator";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
+
+const checkMissingFields = (
+  username,
+  password,
+  name,
+  email,
+  phoneNumber,
+  profilePicture,
+  aboutMe
+) => {
+  const missingFields = [];
+
+  if (!username) {
+    missingFields.push("username");
+  }
+  if (!password) {
+    missingFields.push("password");
+  }
+  if (!name) {
+    missingFields.push("name");
+  }
+  if (!email) {
+    missingFields.push("email");
+  }
+  if (!phoneNumber) {
+    missingFields.push("phoneNumber");
+  }
+  if (!profilePicture) {
+    missingFields.push("profilePicture");
+  }
+  if (!aboutMe) {
+    missingFields.push("aboutMe");
+  }
+
+  return missingFields;
+};
+
+const checkFieldValues = async (email) => {
+  const incorrectFields = [];
+
+  if (!validator.isEmail(email)) {
+    incorrectFields.push("email: email is not formatted correctly");
+  }
+
+  return incorrectFields;
+};
 
 router.post("/", auth, async (req, res, next) => {
   try {
@@ -20,34 +69,36 @@ router.post("/", auth, async (req, res, next) => {
       aboutMe,
     } = req.body;
 
-    const missingFields = [];
-
-    if (!username) {
-      missingFields.push("username");
-    }
-    if (!password) {
-      missingFields.push("password");
-    }
-    if (!name) {
-      missingFields.push("name");
-    }
-    if (!email) {
-      missingFields.push("email");
-    }
-    if (!phoneNumber) {
-      missingFields.push("phoneNumber");
-    }
-    if (!profilePicture) {
-      missingFields.push("profilePicture");
-    }
-    if (!aboutMe) {
-      missingFields.push("aboutMe");
-    }
+    const missingFields = checkMissingFields(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe
+    );
 
     if (missingFields.length > 0) {
       return res
         .status(400)
         .json({ message: `missing fields ${missingFields}` });
+    }
+
+    const incorrectFields = await checkFieldValues(email);
+
+    if (incorrectFields.length > 0) {
+      return res
+        .status(422)
+        .json({ message: `Incorrect values for fields: ${incorrectFields}` });
+    }
+
+    const existingHost = await prisma.host.findFirst({ where: { username } });
+    if (existingHost) {
+      return res.status(422).json({
+        message:
+          "existingHost: username is already in use. username must be unique",
+      });
     }
 
     const newHost = await createHost(
@@ -115,6 +166,15 @@ router.put("/:id", auth, async (req, res, next) => {
       profilePicture,
       aboutMe,
     } = req.body;
+
+    const incorrectFields = await checkFieldValues(email);
+
+    if (incorrectFields.length > 0) {
+      return res
+        .status(422)
+        .json({ message: `Incorrect values for fields: ${incorrectFields}` });
+    }
+
     const host = await updateHostByID(id, {
       username,
       password,
